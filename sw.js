ccaf35caadd57a514b38b4b9d1379ea9a4fbdb8a
mateolpd5 <mateolpd5@gmail.com>
2025-12-21 22:39:02 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = "pwa-cache-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+  /* AGREGA AQUÍ TUS CSS, JS, IMÁGENES */
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
